86c7420a20a7ed56d0d445639fed9c4f24b17abe
mrjohnyhats <jonbutterminecraftian@gmail.com>
2017-05-08 21:21:25 -0700

    made a new exercise featuring the story of Tommy

---
+++ basic_page/exercises/conditional_rendering/jsx.js
@@ -0,0 +1,13 @@
+function getHour(){
+    return Math.floor(Math.random()*24);
+}
+
+/*
+Tommy the porcupine wants to tell his customers when his sand restaurant will be open and wants your help.
+Tommy has already written a function that will tell you the current hour in 24 hour format and now wants your help to display one element when his store is open and another when it isn't.
+Tommy's store is open from 9 to 22.
+If Tommy's website doesn't work properly, no new customers will come to his restaurant, and the server space he bought will bankrupt his sand restaurant!
+Tommy has built his life around this restaurant, and declaring bankruptcy will cut off his only revenue stream.
+Because Tommy has no account, he won't be able to feed his children and will have to rely on the food bank in the library.
+Please help Tommy. Not many animals in the animal kingdom like sand, but Tommy isn't good at making other kinds of food. He has to search far and wide for sand lovers using the internet!
+*/
